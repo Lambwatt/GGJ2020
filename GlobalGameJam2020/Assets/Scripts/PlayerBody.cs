c2cd778a2546243633540ec43ae361ba481533dd
Michael Pattie <mjpattie@gmail.com>
2020-02-01 14:56:33 -0800

    Handled collision issue

---
+++ GlobalGameJam2020/Assets/Scripts/PlayerBody.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class PlayerBody : MonoBehaviour {
+
+    public int HP = 3;
+    public Text HPText;
+    public string HPHeader = "HP: ";
+
+    int Score = 0;
+    public Text ScoreText;
+    public string ScoreHeader = "Score: ";
+
+	// Use this for initialization
+	void Start () {
+        UpdateHP();
+        UpdateScore();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+        string val = collision.tag;
+        if(val == "Rock"){
+            
+            HP--;
+            UpdateHP();
+            if(HP<=0){
+                Die();
+            }
+        }
+	}
+
+    public void AddToScore(int points){
+        Score += points;
+        UpdateScore();
+    }
+
+    void UpdateScore(){
+        ScoreText.text = ScoreHeader + Score;
+    }
+
+    void UpdateHP(){
+        HPText.text = HPHeader + HP;
+    }
+
+    private void Die(){
+        SceneManager.LoadScene("EndScreen");
+    }
+}
--- GlobalGameJam2020/Assets/Scripts/PlayerMovement.cs
+++ GlobalGameJam2020/Assets/Scripts/PlayerMovement.cs
@@ -1,25 +1,20 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
 
-public class Player : MonoBehaviour {
+public class PlayerMovement : MonoBehaviour {
 
     public Rigidbody2D Rb;
     public float Speed;
-    public int HP = 3;
-
-    public Text HPText;
-    public string HPHeader = "HP: ";
 
 	// Use this for initialization
 	void Start () {
-        HPText.text = HPHeader + HP;
+		
 	}
 	
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         Vector2 velocity = new Vector2();
 
         if (Input.GetKey(KeyCode.A))
@@ -44,21 +39,6 @@ public class Player : MonoBehaviour {
 
         transform.rotation = Quaternion.FromToRotation(Vector2.right, velocity);
         Rb.velocity = velocity * Speed;
-	}
-
-	private void OnTriggerEnter2D(Collider2D collision)
-	{
-        string val = collision.tag;
-        if(val == "Rock"){
-            HP--;
-            HPText.text = HPHeader + HP;
-            if(HP<=0){
-                Die();
-            }
-        }
-	}
-
-    private void Die(){
-        SceneManager.LoadScene("EndScreen");
     }
+
 }
