3aafec488d8e5c0d509f74b0e93d620f715f2075
Michael Pattie <mjpattie@gmail.com>
2020-01-31 21:45:43 -0800

    Added Pickup

---
+++ GlobalGameJam2020/Assets/Scripts/GrabRegion.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrabRegion : MonoBehaviour {
+
+    public SpriteRenderer Image;
+
+    List<Grabbable> GrabbedStuff;
+
+	void OnTriggerEnter2D(Collider2D collision)
+	{
+        Debug.Log("Collided!");
+        string val = collision.tag;
+
+        if (val == "Pair")
+        {
+            //check if pair matches an element in the list
+        }
+        else if(val == "Grabbable")
+        {
+            collision.transform.SetParent(transform);
+            //if On Add to list
+            //otherwise don't
+        }
+        else if(val == "Rock")
+        {
+            //Shatter?
+        }
+	}
+}
